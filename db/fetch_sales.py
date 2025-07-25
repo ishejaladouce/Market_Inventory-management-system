82e575271020ec56a7c194da4bf0e714a0a80b1a
ishejaladouce <l.isheja@alustudent.com>
2025-07-25 11:34:18 +0200

    fetch and format all sales records from MYSQL

---
+++ db/fetch_sales.py
@@ -0,0 +1,50 @@
+# This function will connect to the Database and fetches all sales records from the 'sales' table.
+
+#This gives us access to out DB connection function
+from db.db_config import connect_db
+
+def get_sales():
+
+    #connect to the database
+    conn = connect_db()
+
+    if not conn:
+        #If the connection fails, return an empty list
+        return []
+
+    cursor = conn.cursor() #create a cursor object to run SQL queries
+
+    cursor.execute("""
+        SELECT product_name, quantiry, unit_price, total_amount, sale_time
+        FROM sales
+        ORDER BY sale_time DESC
+    """)
+
+    #Fetch all the results from the query
+    rows = cursor.fetchall()
+
+    #Close the cursor and DB connection 
+    cursor.close()
+    conn.close()
+
+    # Format the data in a list of dictionaries
+    sales = []
+    for row in rows:
+        sels.append({
+            "product": row[0],
+            "quantity": row[1],
+            "unit_price": float(row[2]),
+            "total": float(row[3]),
+            "date": row[4].strftime("%Y-%m-%d %H:%M:%S")
+        })
+
+        #return the final list of formatted sales
+    return sales
+
+except Exception as e:
+    # If anything goes Wrong, print the error and return an empty list
+    print("Failed to fetch sales data:", e)
+    return []
+
+
+
