1b2c5645917364ecc3d9811025d13502ae753a77
ishejaladouce <l.isheja@alustudent.com>
2025-07-25 13:41:26 +0200

    Modify the main file

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@
 
 from features.check_inventory import check_inventory  # to display inventory
 from db.fetch_products import get_inventory           # to fetch data from MySQL
+from features.record_sale import record_sale
 
 # Main menu function that keeps running until the user exits
 def main_menu():
@@ -12,21 +13,31 @@ def main_menu():
         print("\n LOCAL MARKET INVENTORY SYSTEM")
         print(" -----------------------------")
         print("1. Check Inventory")
-        print("2. Exit")
+        print("2. Record a Sale")
+        print("3. View Sales Report")
+        print("4. Exit")
 
 
         # Ask for user input
-        choice = input("Choose an option (1-2): ")
+        choice = input("Choose an option (1-4): ")
 
         # If user chooses to check inventory
         if choice == "1":
             inventory = get_inventory()  # fetch products from database
             check_inventory(inventory)   # display them in a table format
 
-        # If user chooses to exit
         elif choice == "2":
+            record_sale()
+
+        elif choice == "3":
+            sales = get_sales()
+            display_sales_report(sales)
+
+        # If user chooses to exit
+        elif choice == "4":
             print("Exiting the system. Goodbye!")
             break
+
 
         # If user enters something invalid
         else:
